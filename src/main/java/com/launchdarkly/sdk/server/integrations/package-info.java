b8449219bdbd26694ef94c1b89283a737f46e488
Ember Stevens <ember.stevens@launchdarkly.com>
2021-09-28 10:43:36 -0700

    Updates docs URLs

--- src/main/java/com/launchdarkly/sdk/server/integrations/package-info.java
+++ src/main/java/com/launchdarkly/sdk/server/integrations/package-info.java
@@ -2,7 +2,7 @@
  * This package provides a Consul-backed data store for the LaunchDarkly Java SDK.
  * <p>
  * For more details about how and why you can use a persistent data store, see:
- * https://docs.launchdarkly.com/v2.0/docs/using-a-persistent-feature-store
+ * https://docs.launchdarkly.com/sdk/features/storing-data/consul#java
  * <p>
  * For details on usage, see {@link com.launchdarkly.sdk.server.integrations.Consul}.
  */
